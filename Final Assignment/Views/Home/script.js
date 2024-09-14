c42fcfeb399add9dc8e33bd49befcb9e44f41436
koseke <koseke@RUK1-5CG14864VQ>
2024-09-14 09:26:59 +0100

    Add project files.

---
+++ Final Assignment/Views/Home/script.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const heading = document.querySelector('h1');
+    let size = 16;
+
+    function animate() {
+        size += 2;
+        heading.style.fontSize = size + 'px';
+        if (size < 50) {
+            requestAnimationFrame(animate);
+        }
+    }
+
+    animate();
+});
